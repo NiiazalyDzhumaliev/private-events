2622145fd463a2bd617693e299cd08eca5c2e6d4
Niiazaly Dzhumaliev <diamondas1993@gmail.com>
2020-06-12 20:23:37 +0600

    Create new, show and create methods in User

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,27 @@
 class UsersController < ApplicationController
+    def def new
+        @user = User.new
+    end
+
+    def show
+        @user = User.find(params[:id])
+    end
+    
+    def create
+        @user = User.new(user_params)
+        if @user.save
+          flash[:success] = "Object successfully created"
+          redirect_to @user
+        else
+          flash[:error] = "Something went wrong"
+          render 'new'
+        end
+    end
+    
+    private
+
+    def user_params
+        params.require(:user).permit(:username)
+    end
+    
 end
