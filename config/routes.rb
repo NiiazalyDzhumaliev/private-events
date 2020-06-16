fff6223499a76e6fd19e9e2aba8b9db0008b0455
hillarioh <okerioh@gmail.com>
2020-06-16 14:06:07 +0300

    add log out link, edit route names

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'signup', to: 'users#new', as: 'signup'
+  get 'login', to: 'sessions#new' as: 'login'
+  get 'logout', to: 'sessions#destroy', as: 'logout' 
   resources :users
-  resources :sessions, only: [:new,:create]
+  resources :sessions, only: [:new,:create,:destroy]
   root "users#index"
 end
