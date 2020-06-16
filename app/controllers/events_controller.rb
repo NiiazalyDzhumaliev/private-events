a11213220a53ecc271c41c63547ec5995b78c52b
Niiazaly Dzhumaliev <diamondas1993@gmail.com>
2020-06-16 20:00:33 +0600

    Add events controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,2 +1,30 @@
 class EventsController < ApplicationController
+    def new
+        @event = Event.new
+    end
+
+    def show
+        @event = Event.find(params[:id])
+    end
+
+    def index
+        @events = Event.all
+    end
+
+    def create
+        @event = Event.new(event_params)
+        if @event.save
+          flash[:success] = "Event successfully created"
+          redirect_to @event
+        else
+          flash[:error] = "Something went wrong"
+          render 'new'
+        end
+    end
+    
+    private
+
+    def event_params
+        params.require(:events).permit(:title, :desc, :date, :location, :creator_id)        
+    end
 end
