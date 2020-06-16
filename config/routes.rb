551dd60f9565f9e7dc94565374933df9f19e4ceb
Niiazaly Dzhumaliev <diamondas1993@gmail.com>
2020-06-16 18:15:24 +0600

    Logout method fixed

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'signup', to: 'users#new', as: 'signup'
-  get 'login', to: 'sessions#new' as: 'login'
+  get 'login', to: 'sessions#new', as: 'login'
   get 'logout', to: 'sessions#destroy', as: 'logout' 
   resources :users
   resources :sessions, only: [:new,:create,:destroy]
