d9b8649e014835f3fa21dae986a38f9e7c7c9b67
Niiazaly Dzhumaliev <diamondas1993@gmail.com>
2020-06-16 18:58:02 +0600

    Replace datetime with date in Events

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_16_123738) do
+ActiveRecord::Schema.define(version: 2020_06_16_125053) do
 
   create_table "events", force: :cascade do |t|
     t.string "title"
     t.text "desc"
     t.string "location"
-    t.datetime "date"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "creator_id"
+    t.date "date"
   end
 
   create_table "users", force: :cascade do |t|
