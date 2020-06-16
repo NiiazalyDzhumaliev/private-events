5e523e251318bcd349da910b86390568feda9c99
hillarioh <okerioh@gmail.com>
2020-06-16 12:08:00 +0300

    Add session logic to create action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,4 +2,17 @@ class SessionsController < ApplicationController
     def new
     end
 
+    def create
+        @user = User.find(params[:username])
+        
+        if user
+            session[:current_user_id] = @user_id       
+            flash[:success] = "Logged in"
+            redirect_to root
+        else
+            flash[:error] = "username invalid"
+            render "new"     
+        end
+    end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,10 @@ class UsersController < ApplicationController
     def show
         @user = User.find(params[:id])
     end
+
+    def index
+        
+    end
     
     def create
         @user = User.new(user_params)
