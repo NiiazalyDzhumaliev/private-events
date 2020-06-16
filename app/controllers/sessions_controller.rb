d1221f289ee36906f6208b87e93ad92504029e85
hillarioh <okerioh@gmail.com>
2020-06-16 13:49:02 +0300

    Add index view, add session in user create action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,13 +6,19 @@ class SessionsController < ApplicationController
         @user = User.find_by_username(params[:username])
         
         if @user
-            session[:current_user_id] = @user_id       
+            session[:current_user_id] = @user.id       
             flash[:success] = "Logged in"
             redirect_to root_path
         else
             flash[:error] = "username invalid"
             render "new"     
         end
+    end
+
+    def destroy
+        session[:current_user_id]=nil
+        flash[:success] = "Logged out"
+        redirect_to root_path
     end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,6 +14,7 @@ class UsersController < ApplicationController
     def create
         @user = User.new(user_params)
         if @user.save
+            session[:current_user_id] = @user.id
           flash[:success] = "User successfully created"
           redirect_to @user
         else
