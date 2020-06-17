ea506c0dc546a8e20c9738466cc7b60669e5241a
Niiazaly Dzhumaliev <diamondas1993@gmail.com>
2020-06-17 20:21:20 +0600

    Change User show page

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -5,7 +5,7 @@ class EventsController < ApplicationController
     end
 
     def show
-        @event = Event.find(params[:id])  
+        @event = Event.find(params[:id])
     end
 
     def index
