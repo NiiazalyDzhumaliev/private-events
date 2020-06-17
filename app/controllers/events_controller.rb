34138bb926e3e9f480368abb70c3a4be57185b98
hillarioh <okerioh@gmail.com>
2020-06-17 14:30:01 +0300

    Ass scope, add partial, edit user show

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -4,7 +4,7 @@ class EventsController < ApplicationController
     end
 
     def show
-        @event = Event.find(params[:id])        
+        @event = Event.find(params[:id])  
     end
 
     def index
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,9 @@
 class Event < ApplicationRecord
+    # require "date" 
     belongs_to :creator, class_name: "User"
     has_many :event_attendances, foreign_key: :attended_event_id
     has_many :attendees, through: :event_attendances, source: :event_attendee
+
+    scope  :upcoming_events, -> { where('date >= ?', Date.today) }
+    scope  :previous_events, -> { where('date < ?', Date.today) }
 end
