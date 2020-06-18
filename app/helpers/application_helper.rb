b03aa1d0543c6f75d568b7cc87234bfdd149d93a
Niiazaly Dzhumaliev <diamondas1993@gmail.com>
2020-06-18 20:24:16 +0600

    Add user_actions method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
+    def user_actions(current_user)
+        html = ''
+    
+        if current_user
+          html += link_to("Profile (#{current_user.username})", current_user)
+          html += ' - '
+          html += link_to('Log Out', logout_path)
+          html += ' - '
+          html += link_to("Create Event", new_event_path)
+        else
+          html += link_to('Sign Up', signup_path)
+          html += ' or '
+          html += link_to('Log In', login_path)
+        end
+        html.html_safe
+      end
 end
