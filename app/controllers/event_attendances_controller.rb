bdd5839edea92703152751e767c9acbf42bc1ce4
hillarioh <okerioh@gmail.com>
2020-06-18 10:42:47 +0300

    add create action, add form in new view

--- app/controllers/event_attendances_controller.rb
+++ app/controllers/event_attendances_controller.rb
@@ -1,5 +1,17 @@
 class EventAttendancesController < ApplicationController
   def new
     @event = Event.find(params[:event_id])
+    @event_attendance = EventAttendance.new
+  end
+
+  def create
+    @event = Event.find(params[:event_id])
+    
+    @event_attendance = @event.event_attendances.create(comment_params)
+    redirect_to article_path(@article)       
+  end
+
+  def att_params
+    params.require(:event_attendance).permit(:event_attendee_id,:attended_event_id)
   end
 end
