9669b4861abfae811a1da11f97a1b9917d3be8e7
Niiazaly Dzhumaliev <diamondas1993@gmail.com>
2020-06-19 16:00:05 +0600

    Add test cases

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -2,6 +2,15 @@ require 'rails_helper'
 
 RSpec.describe Event, :type => :model do
 
+  let(:user) {User.create(username: 'username')}
+  let(:event) {user.events.create(desc: 'New Event', date: '2020-06-28')}
+
+  context 'A created event should belong to a user as its creator' do
+   it 'It should check for an event creator_id and return it' do
+     expect(event.creator.username).to eq(user.username)
+   end
+ end
+ 
   context "Check for validation of event input" do
     it "is valid with valid attributes" do
       expect(Event.create).to_not be_valid
