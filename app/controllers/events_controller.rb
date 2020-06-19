25d8b99120790cf3e131375ccbd7a521f993c9a7
Niiazaly Dzhumaliev <diamondas1993@gmail.com>
2020-06-19 18:18:19 +0600

    Add previous and upcoming events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -10,6 +10,8 @@ class EventsController < ApplicationController
 
   def index
     @events = Event.all
+    @past_events = Event.previous_events
+    @upcoming_events = Event.upcoming_events
   end
 
   def create
