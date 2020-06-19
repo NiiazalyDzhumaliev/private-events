3b7366abdb74b79a65868abad15afaecdddea8e0
hillarioh <okerioh@gmail.com>
2020-06-19 05:17:25 +0300

    edit link location

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,10 +8,14 @@ module ApplicationHelper
           html += link_to('Log Out', logout_path)
           html += ' - '
           html += link_to("Create Event", new_event_path)
+          html += ' - '
+          html += link_to("All Events", root_path)
         else
           html += link_to('Sign Up', signup_path)
           html += ' or '
           html += link_to('Log In', login_path)
+          html += ' - '
+          html += link_to("All Events", root_path)
         end
         html.html_safe
       end
